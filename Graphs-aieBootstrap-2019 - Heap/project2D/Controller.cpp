9c0b8c9d53b1e680b7b410055fb209fd38f861ba
Blake Peck <s210561@students.aie.edu.au>
2021-07-12 12:31:50 +1000

    Trying to come up with a way for when space is pressed the character places the node as a block to stop the Agent and find a new path

--- Graphs-aieBootstrap-2019 - Heap/project2D/Controller.cpp
+++ Graphs-aieBootstrap-2019 - Heap/project2D/Controller.cpp
@@ -1,5 +1,6 @@
 #include "Controller.h"
 #include "Pathfinder.h"
+#include "Input.h"
 
 Controller::Controller(Pathfinder* pPathefinder, Vector2 v2Position)
 {
@@ -13,6 +14,27 @@ Controller::~Controller()
 
 void Controller::Update(float DeltaTime)
 {
+	aie::Input* m_input = aie::Input::GetInstance();
+
+	if (m_input->WasKeyPressed(aie::INPUT_KEY_W))
+	{
+		m_v2Position.y += 20.0f;
+	}
+
+	if (m_input->WasKeyPressed(aie::INPUT_KEY_S))
+	{
+		m_v2Position.y -= 20.0f;
+	}
+
+	if (m_input->WasKeyPressed(aie::INPUT_KEY_A))
+	{
+		m_v2Position.x -= 20.0f;
+	}
+
+	if (m_input->WasKeyPressed(aie::INPUT_KEY_D))
+	{
+		m_v2Position.x += 20.0f;
+	}
 }
 
 void Controller::Draw(aie::Renderer2D* pRenderer)
